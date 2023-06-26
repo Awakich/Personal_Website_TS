b67d4fb97e022b431316bdb9e9e4d3866c74e755
Artem Ivanov <106466193+Awakich@users.noreply.github.com>
2023-06-26 20:33:55 +0300

    Update Contacts.tsx

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -6,7 +6,7 @@ const Contacts: React.FC = () => {
             <h2 className="font-black italic text-8xl">Contacts</h2>
 
             <div className="flex mt-12">
-                <ContactInfo name="Telegram" link="https://t.me/ivanov_prg" />
+                <ContactInfo name="Telegram" link="https://t.me/Ionkiks" />
                 <ContactInfo name="VK" link="https://vk.com/artyomivanov3" />
                 <ContactInfo name="Email" link="artem.bqpower@gmail.com" />
             </div>
